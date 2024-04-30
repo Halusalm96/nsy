c836150840c47a1ce368f4fcdfc1cad75bc5e54b
Halusalm96 <dmadkr6004@naver.com>
2024-04-30 18:04:34 +0900

    3,4강 코드 완성

--- src/main/java/com/jojodu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/jojodu/book/springboot/service/posts/PostsService.java
@@ -2,7 +2,7 @@ package com.jojodu.book.springboot.service.posts;
 
 import com.jojodu.book.springboot.domain.posts.Posts;
 import com.jojodu.book.springboot.domain.posts.PostsRepository;
-//import com.jojodu.book.springboot.web.dto.PostsListResponseDto;
+import com.jojodu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojodu.book.springboot.web.dto.PostsResponseDto;
 import com.jojodu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojodu.book.springboot.web.dto.PostsUpdateRequestDto;
@@ -12,8 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-//import java.util.List;
-//import java.util.stream.Collectors;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,14 +35,14 @@ public class PostsService {
         return id;
     }
 
-//    @Transactional
-//    public void delete (Long id) {
-//        Posts posts = postsRepository.findById(id)
-//                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
-//
-//        postsRepository.delete(posts);
-//    }
-//
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
 //    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
@@ -50,11 +50,11 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
-//
-//    @Transactional(readOnly = true)
-//    public List<PostsListResponseDto> findAllDesc() {
-//        return postsRepository.findAllDesc().stream()
-//                .map(PostsListResponseDto::new)
-//                .collect(Collectors.toList());
-//    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 }
