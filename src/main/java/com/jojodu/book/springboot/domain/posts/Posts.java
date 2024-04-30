8437a4d6ddb8a255e8a4ab310a8d6ac0e9b8395f
Halusalm96 <dmadkr6004@naver.com>
2024-04-30 15:01:50 +0900

    BaseTime

--- src/main/java/com/jojodu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojodu/book/springboot/domain/posts/Posts.java
@@ -3,6 +3,8 @@ package com.jojodu.book.springboot.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -13,7 +15,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
